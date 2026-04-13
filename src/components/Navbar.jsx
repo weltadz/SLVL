681210aa021d7bd9af96efb3f75e4555973aff75
weltadz <lemueltadz@gmail.com>
2026-04-13 15:38:50 +0800

    add folder on pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,25 +2,30 @@ const menuByRole = {
     Employee:[
         {name: "Dashboard"},
         {name: "Request"},
-        {name: "Profile"}
+        {name: "Profile"},
+        {name: "Logout"}
     ],
     Admin:[
         {name: "Dashboard"},
         {name: "Employees"},
-        {name: "Departments"}
+        {name: "Departments"},
+        {name: "Logout"}
     ],
     HR:[
         {name: "Dashboard"},
         {name: "Employees"},
-        {name: "Request"}
+        {name: "Request"},
+        {name: "Logout"}
     ],
     Supervisor:[
         {name: "Dashboard"},
-        {name: "Request"}
+        {name: "Request"},
+        {name: "Logout"}
     ],
     Manager:[
         {name: "Dashboard"},
-        {name: "Request"}
+        {name: "Request"},
+        {name: "Logout"}
     ]
 };
 
@@ -28,11 +33,11 @@ const Navbar = ({role}) =>{
     const menu = menuByRole[role] || [];
 
     return(
-        <nav className="hidden sm:text-sm sm:flex justify-between bg-gray-800 w-full min-h-16 text-white box-border md:text-lg">
+        <nav className="hidden sm:text-sm sm:flex justify-between bg-gray-800 w-full h-14 text-white box-border ">
             <div className="flex justify-center items-center text-2xl box-border p-5">
                 <h1>IKPC SLVL System</h1>
             </div>
-            <div className="flex justify-center items-center gap-5 box-border p-5 ">
+            <div className="flex justify-center items-center gap-5 box-border p-5 pr-5 ">
                 {menu.map((item)=>(
                     <a key={item.name} href="" className="hover:text-lime-400">
                         {item.name}
